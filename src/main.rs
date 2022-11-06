9fd7a57924f48b647348f1018fbcc7f7b9eb021d
raphjaph <raphjaph@protonmail.com>
2022-11-06 22:43:38 +0100

    doing stupid stuff; embeding files into static binary

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use axum::{routing::get, Router};
+use axum::{body, extract::Path, response::Response, routing::get, Router};
+use http::{header, StatusCode};
+use rust_embed::RustEmbed;
+use std::process::Command;
+
+#[derive(RustEmbed)]
+#[folder = "public"]
+struct StaticAssets;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(|| async { "Hello, World!" }));
+    Command::new("zola")
+        .arg("build")
+        .status()
+        .expect("could not build site");
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let router = Router::new().route("/*path", get(static_files));
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(router.into_make_service())
         .await
         .unwrap();
+}
+
+async fn static_files(Path(path): Path<String>) -> Response {
+    let content = StaticAssets::get(if path.ends_with('/') {
+        &(path.clone() + "index.html")
+    } else {
+        &path
+    });
+    // .ok_or_else(|| StatusCode::NOT_FOUND);
+
+    let body = body::boxed(body::Full::from(content.unwrap().data));
+    let mime = mime_guess::from_path(path).first_or_octet_stream();
+
+    Response::builder()
+        .header(header::CONTENT_TYPE, mime.as_ref())
+        .body(body)
+        .unwrap()
 }
